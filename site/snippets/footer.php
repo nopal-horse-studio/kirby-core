590046411f5facbaf0af3fa1c14a82232049ae24
Jorge Fco <jorge.fco.mend@gmail.com>
2018-10-01 21:30:32 -0500

    Configure information contact

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,18 @@
+<?php $contact = page('contact'); ?>
 <footer>
 	<div>
+		<p>
+			<?= $contact->street()->html(); ?>,
+			<?= $contact->colony()->html(); ?>,
+			<?= $contact->cp()->html(); ?>,
+			<?= $contact->city()->html(); ?>,
+			<?= $contact->state()->html(); ?>,
+			<?= $contact->country()->html(); ?>
+		</p>
+		<p><?= $contact->email()->html(); ?></p>
+		<p><?= $contact->phone()->html(); ?></p>
+		<p><?= $contact->telephone()->html(); ?></p>
+		<p><?= $contact->whatsapp()->html(); ?></p>
 	</div>
 </footer>
 
